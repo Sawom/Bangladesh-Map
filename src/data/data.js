7cf2f6bea8a9903df6c2eccb15095dff56e040d6
Sawom <asawom250@gmail.com>
2026-02-13 14:34:23 +0600

    sample json data added and showed

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+
+export const districtsData = {
+  rajshahi: { 
+    name: "Rajshahi", 
+    x: 300, 
+    y: 850, 
+    info: "রেশম ও আমের জন্য বিখ্যাত এই শহরটি পদ্মা নদীর তীরে অবস্থিত।" 
+  },
+  nilphamari: { 
+    name: "nilphamari", 
+    x: 750, 
+    y: 1050, 
+    info: "" 
+  },
+  panchagarh: { 
+    name: "panchagarh", 
+    x: 750, 
+    y: 1050, 
+    info: "" 
+  },
+  
+};
