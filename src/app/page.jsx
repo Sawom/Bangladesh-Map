f189e1585808cc05d51866428976869ebfea4b5f
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2026-02-14 19:59:32 +0600

    fix(ui): correct background color and modal trigger issues
    - Resolve top background color issue
    - Fix text click event not opening modal
    - Refactor BangladeshMap to self-closing component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="bg-gray-100">
       <div className="container mx-auto text-center">
-        <h1 className="text-4xl mt-4 md:text-6xl  font-black text-teal-900 mb-4 tracking-tighter ">
+        <h1 className="text-4xl pt-4 md:text-6xl  font-black text-teal-900 mb-4 tracking-tighter ">
           BANGLADESH
           <span className="text-teal-500 not-italic mx-4">EXPLORER</span>
         </h1>
@@ -13,7 +13,7 @@ export default function Home() {
         </p>
       </div>
 
-      <BangladeshMap></BangladeshMap>
+      <BangladeshMap/>
     </div>
   );
 }
